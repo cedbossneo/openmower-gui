6763235562fa03fe10488fb22d18cdd894d6fc0e
Cedric Hauber <chauber@onewealthplace.com>
2023-07-18 19:33:42 +0200

    Fix proxy port

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
@@ -7,7 +7,7 @@ export default defineConfig({
     server: {
         proxy: {
             '/api': {
-                target: 'http://localhost:8080',
+                target: 'http://localhost:4006',
             }
         }
     }
