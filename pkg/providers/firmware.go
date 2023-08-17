77a246fb1977ccb1ca47a58271e93b93557b1562
Cedric Hauber <chauber@onewealthplace.com>
2023-08-17 13:47:44 +0200

    Fix GPIO on vermut board for flashing

--- pkg/providers/firmware.go
+++ pkg/providers/firmware.go
@@ -165,7 +165,7 @@ func (fp *FirmwareProvider) flashVermut(writer io.Writer, config FirmwareConfig)
 	_, _ = writer.Write([]byte("------> Firmware unzipped\n"))
 
 	_, _ = writer.Write([]byte("------> Flashing firmware...\n"))
-	cmd = execabs.Command("/bin/bash", "-c", "openocd -f interface/raspberrypi-swd.cfg -f target/rp2040.cfg -c \"program "+os.TempDir()+"/firmware/firmware/"+config.Version+"/firmware.elf verify reset exit\"")
+	cmd = execabs.Command("/bin/bash", "-c", "echo \"10\" > /sys/class/gpio/export && echo \"out\" > /sys/class/gpio/gpio10/direction && echo \"1\" > /sys/class/gpio/gpio10/value && openocd -f interface/raspberrypi-swd.cfg -f target/rp2040.cfg -c \"program "+os.TempDir()+"/firmware/firmware/"+config.Version+"/firmware.elf verify reset exit\"")
 	cmd.Stdout = writer
 	cmd.Stderr = writer
 	err = cmd.Run()
