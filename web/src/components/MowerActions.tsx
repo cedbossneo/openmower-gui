1819b6817317420b4b9413e86277afd6ed4e68a6
Cedric Hauber <chauber@onewealthplace.com>
2023-08-23 17:26:02 +0200

    openmower-gui-8 add possibility to trigger manual pause mowing from the UI

--- web/src/components/MowerActions.tsx
+++ web/src/components/MowerActions.tsx
@@ -31,57 +31,117 @@ export const MowerActions: React.FC<React.PropsWithChildren<{ api: NotificationI
             })
         }
     };
-    let actionMenuItems = [
+    let actionMenuItems: {
+        key: string,
+        label: string,
+        actions: { command: string, args: any }[],
+        danger?: boolean
+    }[] = [
         {
             key: "mower_s1",
             label: "S1",
+            actions: [{
+                command: "high_level_control",
+                args: {
+                    Command: 3,
+                }
+            }]
         },
         {
             key: "mower_s2",
             label: "S2",
+            actions: [{
+                command: "high_level_control",
+                args: {
+                    Command: 4,
+                }
+            }]
+        },
+        {
+            key: "pause",
+            label: "Pause",
+            actions: [{
+                command: "mower_logic", args: {
+                    Config: {
+                        Bools: [{
+                            Name: "manual_pause_mowing",
+                            Value: true
+                        }]
+                    }
+                }
+            }]
+        },
+        {
+            key: "continue",
+            label: "Continue",
+            actions: [{
+                command: "mower_logic", args: {
+                    Config: {
+                        Bools: [{
+                            Name: "manual_pause_mowing",
+                            Value: false
+                        }]
+                    }
+                }
+            }, {
+                command: "high_level_control",
+                args: {
+                    Command: 1,
+                }
+            }]
         },
         {
             "key": "emergency_off",
             "label": "Emergency Off",
             "danger": true,
+            actions: [{
+                command: "emergency",
+                args: {
+                    Emergency: 0,
+                }
+            }]
         },
         {
             "key": "mow_forward",
             "label": "Blade Forward",
+            actions: [{
+                command: "mow_enabled",
+                args: {MowEnabled: 1, MowDirection: 0}
+            }]
         },
         {
             "key": "mow_backward",
             "label": "Blade Backward",
+            actions: [{
+                command: "mow_enabled",
+                args: {MowEnabled: 1, MowDirection: 1}
+            }]
         },
         {
             "key": "mow_off",
             "label": "Blade Off",
             "danger": true,
+            actions: [{
+                command: "mow_enabled",
+                args: {MowEnabled: 0, MowDirection: 0}
+            }]
         },
     ];
     return <ActionsCard title={"Actions"} size={"small"}>
         {props.children}
         {props.children ? <Divider type={"vertical"} style={{marginRight: 20}}/> : null}
-        <AsyncButton size={"small"} type="primary" onAsyncClick={handleMowerCommand("mower_start")}
+        <AsyncButton size={"small"} type="primary" onAsyncClick={handleMowerCommand("high_level_control", {Command: 1})}
                      style={{marginRight: 10}}>Start</AsyncButton>
-        <AsyncButton size={"small"} onAsyncClick={handleMowerCommand("mower_home")}
+        <AsyncButton size={"small"} onAsyncClick={handleMowerCommand("high_level_control", {Command: 2})}
                      style={{marginRight: 10}}>Home</AsyncButton>
-        <AsyncButton danger size={"small"} onAsyncClick={handleMowerCommand("emergency", {emergency: 1})}
+        <AsyncButton danger size={"small"} onAsyncClick={handleMowerCommand("emergency", {Emergency: 1})}
                      style={{marginRight: 10}}>Emergency On</AsyncButton>
         <AsyncDropDownButton style={{display: "inline"}} size={"small"} menu={{
             items: actionMenuItems,
-            onAsyncClick: (e) => {
-                switch (e.key) {
-                    case "emergency_off":
-                        return handleMowerCommand("emergency", {emergency: 0})();
-                    case "mow_forward":
-                        return handleMowerCommand("mow", {mow_enabled: 1, mow_direction: 0})();
-                    case "mow_backward":
-                        return handleMowerCommand("mow", {mow_enabled: 1, mow_direction: 1})();
-                    case "mow_off":
-                        return handleMowerCommand("mow", {mow_enabled: 0})();
-                    default:
-                        return handleMowerCommand(e.key)();
+            onAsyncClick: async (e) => {
+                const item = actionMenuItems.find(item => item.key == e.key)
+                for (const action of (item?.actions ?? [])) {
+                    await handleMowerCommand(action.command, action.args)();
                 }
             }
         }}>
